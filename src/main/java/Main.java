9437614f7a986808e3b02044c61e09d7371e5d3a
ahmetcan.ersoz <ahmetcan.ersoz@ulakhaberlesme.com.tr>
2018-12-25 10:17:00 +0300

    Commit 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git Push Test");
         System.out.println("Git Push Test2");
+        System.out.println("Git Push Test3");
     }
 }
